0126d7f3923f1b4f9acea3698cf334d93cedabf6
mrrizal <rizalubuntuuser@gmail.com>
2023-04-30 00:17:30 +0800

    update main func

--- main.go
+++ main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
-	"database/sql"
+	"fmt"
+	"log"
+	"tabungan-api/app/configs"
 	"tabungan-api/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type AppInstance struct {
 	app *fiber.App
-	db  *sql.DB
+	db  *pgxpool.Pool
 }
 
-func NewAppInstance() AppInstance {
-	return AppInstance{app: fiber.New(), db: &sql.DB{}}
+func NewAppInstance(config configs.Config) AppInstance {
+	db, err := configs.NewDB(config)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	app := fiber.New()
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
+	return AppInstance{app: app, db: db}
 }
 
 func main() {
-	app := NewAppInstance()
+	config := configs.LoadConfig()
+	app := NewAppInstance(config)
+
 	routes.SetupRoutes(app.app, app.db)
+	app.app.Listen(fmt.Sprintf(":%s", config.Port))
 }
