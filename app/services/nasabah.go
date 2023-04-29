aa0296639dd23805833ca953b6d3489edd5d7e50
mrrizal <rizalubuntuuser@gmail.com>
2023-04-30 00:16:05 +0800

    add nasabah service

--- app/services/nasabah.go
+++ app/services/nasabah.go
@@ -1,22 +1,45 @@
 package services
 
 import (
-	"database/sql"
+	"errors"
 	"tabungan-api/app/models"
 	"tabungan-api/app/repositories"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type NasabahService struct {
 	NasabahRepository repositories.NasabahRepository
 }
 
-func NewNasabahService(db *sql.DB) NasabahService {
+func NewNasabahService(db *pgxpool.Pool) NasabahService {
 	return NasabahService{
 		NasabahRepository: repositories.NewNasabahRepository(db),
 	}
 }
 
-func (this *NasabahService) Daftar(nasabah *models.Nasabah) error {
-	// todo: do bussiness logic here like check nik and no_hp, also return the no_rekening
-	return nil
+func (this *NasabahService) Daftar(nasabah models.Nasabah) (int, error) {
+	nikIsExists, err := this.NasabahRepository.IsExists("nik", nasabah.Nik)
+	if err != nil {
+		return 0, err
+	}
+
+	if nikIsExists {
+		return 0, errors.New("nik already exists")
+	}
+
+	noHpIsExists, err := this.NasabahRepository.IsExists("no_hp", nasabah.NoHp)
+	if err != nil {
+		return 0, err
+	}
+
+	if noHpIsExists {
+		return 0, errors.New("no_hp already exists")
+	}
+
+	nasabahID, err := this.NasabahRepository.Daftar(nasabah)
+	if err != nil {
+		return 0, err
+	}
+	return nasabahID, nil
 }
