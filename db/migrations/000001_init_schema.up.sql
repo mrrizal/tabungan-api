131d825c7b549e09d9021bcf57706de9c503bc07
mrrizal <rizalubuntuuser@gmail.com>
2023-04-29 23:43:39 +0800

    update migration script

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -4,7 +4,7 @@ CREATE TYPE "kodetransaksi" AS ENUM (
 );
 
 CREATE TABLE "nasabah" (
-  "id" integer PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "nama" varchar,
   "nik" varchar(16) NOT NULL,
   "no_hp" varchar(13) NOT NULL,
@@ -13,13 +13,13 @@ CREATE TABLE "nasabah" (
 
 CREATE TABLE "rekening" (
   "no_rekening" varchar(10) PRIMARY KEY,
-  "user_id" int,
+  "nasabah_id" int,
   "saldo" float NOT NULL,
   "created_at" timestamp DEFAULT (now())
 );
 
 CREATE TABLE "transaksi" (
-  "id" integer PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "no_rekening" varchar(10),
   "nominal" float NOT NULL,
   "kode_transaksi" kodetransaksi,
@@ -34,6 +34,6 @@ CREATE UNIQUE INDEX ON "rekening" ("no_rekening");
 
 CREATE INDEX "kode_transaksi_idx" ON "transaksi" ("kode_transaksi");
 
-ALTER TABLE "rekening" ADD FOREIGN KEY ("user_id") REFERENCES "nasabah" ("id");
+ALTER TABLE "rekening" ADD FOREIGN KEY ("nasabah_id") REFERENCES "nasabah" ("id");
 
 ALTER TABLE "transaksi" ADD FOREIGN KEY ("no_rekening") REFERENCES "rekening" ("no_rekening");
