1e29a100bd34be58f043ec2217705eddfe73ae59
mrrizal <rizalubuntuuser@gmail.com>
2023-04-30 00:18:20 +0800

    add rekening service

---
+++ app/services/rekening.go
@@ -0,0 +1,27 @@
+package services
+
+import (
+	"tabungan-api/app/repositories"
+	"tabungan-api/utils"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type RekeningService struct {
+	RekeningRepository repositories.RekeningRepository
+}
+
+func NewRekeningService(db *pgxpool.Pool) RekeningService {
+	return RekeningService{
+		RekeningRepository: repositories.NewRekeningRepository(db),
+	}
+}
+
+func (this *RekeningService) Daftar(nasabahID int) (string, error) {
+	noRekening := utils.GenerateAccountNumber(10)
+	if err := this.RekeningRepository.Daftar(nasabahID, noRekening); err != nil {
+		return "", err
+	}
+
+	return noRekening, nil
+}
