123068726300f4ecc81c5715e28b9ad95142a1c4
mrrizal <rizalubuntuuser@gmail.com>
2023-04-30 22:49:37 +0800

    add mutasi endpoint

--- app/controllers/transaksi_controller.go
+++ app/controllers/transaksi_controller.go
@@ -8,7 +8,8 @@ import (
 )
 
 type TransaksiController struct {
-	RekeningService services.RekeningService
+	RekeningService  services.RekeningService
+	TransaksiService services.TransaksiService
 }
 
 func (this *TransaksiController) Tabung(c *fiber.Ctx) error {
@@ -20,6 +21,13 @@ func (this *TransaksiController) Tabung(c *fiber.Ctx) error {
 
 	saldo, err := this.RekeningService.Tabung(request)
 	if err != nil {
+		var result models.ErrorResponse
+		result.Remark = err.Error()
+		c.Status(400)
+		return c.JSON(result)
+	}
+
+	if err := this.TransaksiService.Transaksi(request); err != nil {
 		var result models.ErrorResponse
 		result.Remark = err.Error()
 		c.Status(400)
@@ -47,8 +55,38 @@ func (this *TransaksiController) Tarik(c *fiber.Ctx) error {
 		return c.JSON(result)
 	}
 
+	if err := this.TransaksiService.Transaksi(request); err != nil {
+		var result models.ErrorResponse
+		result.Remark = err.Error()
+		c.Status(400)
+		return c.JSON(result)
+	}
+
 	var resp models.TransaksiResponseOk
 	resp.Saldo = saldo
 	c.Status(200)
 	return c.JSON(resp)
+}
+
+func (this *TransaksiController) Mutasi(c *fiber.Ctx) error {
+	noRekening := c.Params("no_rekening")
+	if !this.RekeningService.IsExists(noRekening) {
+		var result models.ErrorResponse
+		result.Remark = "nomor rekening doesn't exists"
+		c.Status(400)
+		return c.JSON(result)
+	}
+
+	mutasiRekening, err := this.TransaksiService.Mutasi(noRekening)
+	if err != nil {
+		var result models.ErrorResponse
+		result.Remark = err.Error()
+		c.Status(400)
+		return c.JSON(result)
+	}
+
+	var result models.MutasiResp
+	result.Results = mutasiRekening
+	c.Status(200)
+	return c.JSON(result)
 }
